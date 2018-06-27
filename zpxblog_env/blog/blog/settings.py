10ae20001cfd3eab637b51d2c2ee514e592d05e2
zhangpengxuan <200922702@qq.com>
2018-06-27 10:47:10 +0800

    django create

--- zpxblog_env/blog/blog/settings.py
+++ zpxblog_env/blog/blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')^f53wr)vcwl-_2i9!kski7)hd5ngp2b!*o&8c$lae7xw!n-xl'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
